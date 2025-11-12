09730ce4018241cdf495ad50688c713e34cd6ec0
Miguel L Carvalho <larissa_lissalopes@hotmail.com>
2025-11-12 09:47:09 -0300

    Check for jp2a before running the script
    Added a check for jp2a installation before execution.

--- Sybau.sh
+++ Sybau.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-jp2a --colors -b Sybau.jpg
+if command -v jp2a >/dev/null 2>&1 ; then
+        echo "jp2a found, proceeding with script"
+        jp2a --colors -b Sybau.jpg
+else
+        echo "jp2a not found, executing installation"
+        sudo pacman -S jp2a
+fi
+
